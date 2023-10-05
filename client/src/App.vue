2e08d9c4f000814155ca98bc6074c71c1dd44dbf
Sessiom <caufaglj2@newpaltz.edu>
2023-10-05 15:01:34 -0400

    update

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,16 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+import navbarVue from './components/navbar.vue';
 </script>
 
 <template>
   <header>
+    <navbar />
     <span class=" fas fa-bars"></span>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="container">
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
     </div>
   </header>
 
@@ -25,6 +21,11 @@ import HelloWorld from './components/HelloWorld.vue'
 header {
   line-height: 1.5;
   max-height: 100vh;
+}
+
+.container {
+  background-color: white;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
 .logo {
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -4,6 +4,7 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="title">Home</h1>>
+    <h2 class="subtitle">Welcome to the home page!</h2>
   </main>
 </template>
