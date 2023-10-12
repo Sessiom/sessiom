d00ac05700fb456db57f5fc7d2ec5bc78a0a67c7
Sessiom <caufaglj2@newpaltz.edu>
2023-10-12 15:18:07 -0400

    fixed bugs

---
+++ client/src/components/loginBadge.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const user = {
+firstName: 'John',
+lastName: 'Doe',
+email: 'john@doe.com',
+
+}
+</script>
+
+<template>
+    <div class="has-text-right"> v-if="user">
+    welcome, {{ user.firstName }} {{ user.lastName }}<br>
+    
+
+    </div>
+</template>
---
+++ client/src/model/sessions.ts
@@ -0,0 +1,11 @@
+import { reactive } from "vue";
+
+const session = reactive ({ 
+
+})
+
+import interface user{
+    
+}
+
+
