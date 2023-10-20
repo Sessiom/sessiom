ee2d21c1832b63dd98b16eb22f877098184f0954
Sessiom <caufaglj2@newpaltz.edu>
2023-10-19 22:25:09 -0400

    bugfix

--- client/src/components/loginBadge.vue
+++ client/src/components/loginBadge.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { getSession, useLogin} from '@/model/session'
+  import { getSession, useLogin } from '@/model/session'
 
   const session = getSession()
   const { login, logout } = useLogin()
@@ -12,48 +12,7 @@
     logout();
   }
 
-</script><script setup lang="ts">
-import { getSession, useLogin } from '@/model/session'
-
-const session = getSession()
-const { login, logout } = useLogin()
-
-const doLogin = () => {
-  login('hbingley1@plala.or.jp', 'CQutx25i8r')
-}
-
-const doLogout = () => {
-  logout();
-}
-
 </script>
-
-<template>
-<div class="has-text-right" v-if="session.user">
-  Welcome, {{ session.user.firstName }} {{ session.user.lastName }} <br>
-  <small>
-    {{ session.user.email }}
-    <a class="button is-small is-light is-warning" @click.prevent="doLogout">
-      <span class="icon">
-        <i class="fas fa-sign-out-alt"></i>
-      </span>
-    </a>
-  </small>
-</div>
-<div class="buttons" v-else>
-  <a class="button is-primary">
-    <strong>Sign up</strong>
-  </a>
-  <a class="button is-light" @click.prevent="doLogin">
-    Log in
-  </a>
-</div>
-</template>
-
-
-<style scoped>
-
-</style>
 
 <template>
   <div class="has-text-right" v-if="session.user">
